61dd5ec7cb6bbf7cbf37d29381f98a4ac008e9fd
Junio Goulart <juniocmgoulart@gmail.com>
2022-07-20 10:15:27 -0300

    alterações

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Media, MediaObject } from '@ionic-native/media/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+    calmaFile: MediaObject;
+    uiFile: MediaObject;
+    gostaFile: MediaObject;
+    calma = '/android_asset/public/assets/que-e-isso-meu-filho-calma.wav';
+    ui = '/android_asset/public/assets/ui-rodrigo-faro.wav';
+    gosta = '/android_asset/public/assets/tmpd9mca4be.wav';
 
-  constructor() {}
+    constructor(
+        public platform: Platform, private media: Media
+    ) { }
+
+    ngOnInit(): void {
+        this.calmaFile = this.media.create(this.calma);
+        this.gostaFile = this.media.create(this.gosta);
+        this.uiFile = this.media.create(this.ui);
+    }
+
+    som1(): void {
+        this.calmaFile.play();
+    }
+
+    som2(): void {
+        this.uiFile.play();
+    }
+
+    som3(): void {
+        this.gostaFile.play();
+    }
 
 }
